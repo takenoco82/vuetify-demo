2be32501ec609f48309cff973754651ec4e6e8bb
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-08-27 21:02:46 +0900

    [add] Login画面を追加

--- demo-project/src/App.vue
+++ demo-project/src/App.vue
@@ -15,9 +15,11 @@
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link>
+    </div>
+    <router-view/>
   </v-app>
 </template>
 
@@ -26,9 +28,6 @@ import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
   data: () => ({
     //
   }),
