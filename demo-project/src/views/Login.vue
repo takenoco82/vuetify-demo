97c61e3660d3ba53f655ed3e17f2f40fa6135bb4
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-08-27 21:16:02 +0900

    [fix] ログインページのフォームを作成

--- demo-project/src/views/Login.vue
+++ demo-project/src/views/Login.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-      Login
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <ul>
+          <li><input placeholder="username" v-model="username"></li>
+          <li><input placeholder="password" v-model="password"></li>
+          <li><button type="submit">login</button></li>
+        </ul>
+      </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    login() {
+      console.log("login clicked");
+      console.log(`username=${this.username},password=${this.password}`);
+
+      // TODO ログインモジュールを実行する
+    }
+  }
 }
 </script>
